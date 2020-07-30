eec803b8b992b493f37df6fc17af4a246d06fe83
Zsolt Nagy <zsolt.nagy@vodafone.com>
2020-07-30 11:33:10 +0200

    modified footer links, added helptext, partners

--- vodafone-global-storefront-theme/config/index.js
+++ vodafone-global-storefront-theme/config/index.js
@@ -3,8 +3,8 @@
 const config = {
 	env: "dev",
 	theme: "vodafone",
-	marketplacePath: "https://testvfgermany.appdirect.com",
-	pageObjectsPath: "https://testvfgermany.appdirect.com/api/internal/storefront/v1",
+	marketplacePath: "https://testvfgroup.appdirect.com",
+	pageObjectsPath: "https://testvfgroup.appdirect.com/api/internal/storefront/v1",
 	publicCSSGenPath: "/assets/css/pages",
 	publicJSPath: "/assets/css/images",
 	publicImagesPath: "/assets/js",
