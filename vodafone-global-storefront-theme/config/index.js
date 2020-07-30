5c5f4f3b3223d1dcb8454a753175ca0ce3e5a1e2
Zsolt Nagy <zsolt.nagy@vodafone.com>
2020-07-30 11:13:56 +0200

    init commit

---
+++ vodafone-global-storefront-theme/config/index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const config = {
+	env: "dev",
+	theme: "vodafone",
+	marketplacePath: "https://testvfgermany.appdirect.com",
+	pageObjectsPath: "https://testvfgermany.appdirect.com/api/internal/storefront/v1",
+	publicCSSGenPath: "/assets/css/pages",
+	publicJSPath: "/assets/css/images",
+	publicImagesPath: "/assets/js",
+	supportedFontExtensions: ["woff"]
+}
+
+module.exports = config
